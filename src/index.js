4edc04771de7ea0d3d8d4b747a7babb4f03a795e
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-25 15:02:51 +0200

    END

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
     <ul>
+      <li>Noodles</li>
       <li>Bacon</li>
       <li>Jamon</li>
-      <li>Noodles</li>
     </ul>
+    <div>
+      <img
+        className="food-img"
+        src="https://kontaktgrillrezepte.de/wp-content/uploads/Gegrillter-Bacon-1024x768.jpeg"
+      />
+      <img
+        className="food-img"
+        src="http://www.jamon.com/images/content/image-jamon-artisanal-hams.jpg"
+      />
+      <img
+        className="food-img"
+        src="https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodlesH2.jpg"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
